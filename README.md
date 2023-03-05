40dc7338dcd9488aa587749e4e256a46599faf2a
Loboss Brunet <98391380+Loboss2206@users.noreply.github.com>
2023-03-05 18:53:52 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# Javascript_TD1
+# M413 - TD1 : Réponses aux Questions
+
+## 6 - Exercice 1
+
+### 6.1 La propriété « document.title »
+
+- Ce sera le chargement de la page
+- document.getElementById("title")
+- textContent
+
+- document.querySelector("h2")
+
+- var h2Count = document.querySelectorAll("h2").length
+
+- document.querySelectorAll(".firstOrLast")
+- J'ai testé grâce à la taille du tableau renvoyé par la méthode ci-dessus
+
+### 6.2 Les propriétés innerHTML, innerText et textContent
+
+Les propriétés innerHTML, innerText et textContent sont toutes des propriétés de l'objet DOM (Document Object Model) et permettent de manipuler le contenu HTML d'un élément. Cependant, elles ont des comportements différents :
+
+- innerHTML : permet de récupérer ou de définir le HTML interne d'un élément sous forme de chaîne de caractères. Les balises HTML sont interprétées.
+
+- innerText : permet de récupérer ou de définir le texte brut interne d'un élément, sans interpréter les balises HTML. Il en résulte que les éléments HTML sont simplement affichés en tant que texte brut.
+
+- textContent : permet de récupérer ou de définir le contenu brut de tous les noeuds textuels enfants d'un élément, sans interpréter les balises HTML. Les noeuds textuels sont simplement concaténés pour former une chaîne de caractères.
+
+### 6.3 La propriété document.lastModified
+
+On prend ce code :
+```javascript
+var metaTags = document.getElementsByTagName("meta");
+	var metaTagsCount = metaTags.length;
+	for (var i = 0; i < metaTagsCount; i++) {
+		if (metaTags[i].getAttribute("name") == "author") {
+			var author = metaTags[i].getAttribute("content");
+			break;
+		}
+	}
+```
+
+- Si on veut sélectionner le 1er auteur de la liste, on laisse le break
+- Si on veut sélectionner le dernier auteur de la liste, on enlève le break
+
+## 7 - Exercice 2 ##################################################
+
+```javascript
+var today = new Date();
+var targetDate = new Date(2023, 6, 19);
+var timeDiff = targetDate.getTime() - today.getTime();
+var nbDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+```
+
+```javascript
+if (nbDays == 1) {
+		var text = "Il reste 1 jour avant le 19 juillet 2023";
+}
+else {
+	var text = "Il reste " + nbDays + " jours avant le 19 juillet 2023";
+}
+
+document.getElementById("daysLeft").innerHTML = text;
+```
+
+### 7.1 - setInterval() et setTimeout()
+
+- On va plûtot utiliser la méthode setInterval car la méthode setTimeout est appelée une seule fois alors que nous voulons que l'heure change continuellement
+
+## 8 - Exercice 3
+
+### 8.1 - Champ Texte et Couleur d’arrière-plan
+
+- J'ai utilisé l'evenement "input" afin d'agir quand le champ texte était modifié
+
+- J'ai utilisé les classes fournies, notemment en utilisant "classList.remove" et "classList.add" pour ajouter et retirer des classes à mon input
+
+
+
+
+
+
+
+
---
+++ js/td1.js
@@ -0,0 +1,310 @@
+'use strict';
+
+// M413 - TD1
+let message = 'JavaScript is ok :)';
+// alert( message);
+console.log(message);
+
+function defineHeading1() {
+	document.title = document.getElementById("title").textContent;
+}
+
+function defineHeading2() {
+	document.title = document.querySelector("h2").textContent;
+}
+
+function defineHeading3() {
+	var h2Count = document.querySelectorAll("h2").length;
+	if (h2Count == 0) {
+		document.title = "Brunet Logan";
+	}
+	else {
+		document.title = document.querySelectorAll("h2")[h2Count - 1].textContent;
+	}
+}
+
+function defineHeading4() {
+	var elements = document.querySelectorAll(".firstOrLast");
+	var elementsCount = elements.length;
+	if (elementsCount == 0) {
+		document.title = "Brunet Logan";
+	}
+	else if (elementsCount % 2 == 0) {
+		document.title = elements[0].textContent;
+	}
+	else {
+		document.title = elements[elementsCount - 1].textContent;
+	}
+}
+
+function swapInnerHTML() {
+	var elementsP = document.querySelectorAll("p");
+	var temp = elementsP[0].textContent;
+	elementsP[0].innerHTML = elementsP[1].textContent;
+	elementsP[1].innerHTML = temp;
+}
+
+function dateAlter() {
+	var date = new Date();
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	var formattedDate = date.toLocaleDateString('fr-FR', options);
+
+	var elements = document.querySelectorAll("div");
+	var elementsCount = elements.length;
+	var div = elements[elementsCount - 1];
+
+	var metaTags = document.getElementsByTagName("meta");
+	var metaTagsCount = metaTags.length;
+	for (var i = 0; i < metaTagsCount; i++) {
+		if (metaTags[i].getAttribute("name") == "author") {
+			var author = metaTags[i].getAttribute("content");
+			break;
+		}
+	}
+
+	div.innerHTML = "Dernière modification : le " + formattedDate + " par " + author;
+}
+
+function getNbDays() {
+	var today = new Date();
+	var targetDate = new Date(2023, 6, 19);
+	var timeDiff = targetDate.getTime() - today.getTime();
+	var nbDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+	return nbDays;
+}
+
+function changeTextDays() {
+	var nbDays = getNbDays();
+	if (nbDays == 1) {
+		var text = "Il reste 1 jour avant le 19 juillet 2023";
+	}
+	else {
+		var text = "Il reste " + nbDays + " jours avant le 19 juillet 2023";
+	}
+
+	document.getElementById("daysLeft").innerHTML = text;
+}
+
+function updateClock1() {
+	const intervalID = window.setInterval(changeHour, 1000);
+}
+
+function updateClock2() {
+	const intervalID = window.setTimeout(changeHour, 1000);
+}
+
+function updateGraphicClock() {
+	const intervalID = window.setInterval(changeGraphicHour, 1000);
+}
+
+function changeHour() {
+	var date = new Date();
+	document.getElementById("clock").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+function changeGraphicHour() {
+	var block = document.getElementById("graphic-clock");
+
+	var date = new Date();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+
+	const hoursLength = hours.toString().length;
+	const minutesLength = minutes.toString().length;
+
+	var h1;
+	var h2;
+	var m1;
+	var m2;
+
+	if (hoursLength == 1) {
+		h1 = 0;
+		h2 = hours;
+	}
+	else {
+		h1 = hours.toString().substring(0, 1);
+		h2 = hours.toString().substring(1, 2);
+	}
+
+	if (minutesLength == 1) {
+		m1 = 0;
+		m2 = minutes;
+	}
+	else {
+		m1 = minutes.toString().substring(0, 1);
+		m2 = minutes.toString().substring(1, 2);
+	}
+
+	block.innerHTML = "<img src=\"assets/images/" + h1 + ".gif\"></img>" +
+		"<img src=\"assets/images/" + h2 + ".gif\"></img>" +
+		"<img src=\"assets/images/minus.gif\"></img>" +
+		"<img src=\"assets/images/" + m1 + ".gif\"></img>" +
+		"<img src=\"assets/images/" + m2 + ".gif\"></img>";
+}
+
+function onLoad() {
+	console.log('In onLoad() function…');
+	defineHeading4();
+	swapInnerHTML();
+	dateAlter();
+	updateClock1();
+	updateGraphicClock();
+	setUpMenu();
+	setUpInput();
+}
+
+function setUpInput() {
+	var inputText = document.getElementById("inputText");
+
+	inputText.addEventListener("input", function () {
+		var textValue = inputText.value;
+
+		if (textValue === "") {
+			inputText.classList.remove("red");
+			inputText.classList.remove("green");
+			inputText.classList.add("white");
+		} else if (isNaN(textValue)) {
+			inputText.classList.remove("green");
+			inputText.classList.add("red");
+		} else {
+			inputText.classList.remove("red");
+			inputText.classList.add("green");
+		}
+	});
+}
+
+function setUpMenu() {
+	const menuElement = document.querySelectorAll('.menu-element');
+
+	menuElement.forEach(element => {
+		const menuTitle = element.children.item(0);
+		const menuList = element.children.item(1);
+
+		const title = menuTitle.children.item(0);
+		var iconPlus;
+
+		if (menuTitle.children.length == 1) {
+			menuList.style.display = "none";
+			iconPlus = document.createElement('i');
+			iconPlus.classList.add('fas', 'fa-plus');
+			menuTitle.insertBefore(iconPlus, title);
+
+			menuTitle.addEventListener('click', () => {
+				if (menuList.style.display === 'none') {
+					menuList.style.display = 'block';
+					menuTitle.classList.add('clicked');
+					iconPlus.classList.remove('fa-plus');
+					iconPlus.classList.add('fa-minus');
+				} else {
+					menuList.style.display = 'none';
+					menuTitle.classList.remove('clicked');
+					iconPlus.classList.remove('fa-minus');
+					iconPlus.classList.add('fa-plus');
+				}
+			});
+		}
+		else {
+			iconPlus = menuTitle.children.item(0);
+		}
+	})
+}
+
+function search() {
+	var searchText = document.getElementById('search-text').value;
+	clearSelection();
+
+	function clearSelection() {
+		var selected = document.querySelectorAll('.select');
+		for (var i = 0; i < selected.length; i++) {
+			selected[i].classList.remove('select');
+		}
+	}
+
+	if (!searchText == "") {
+
+		function traverse(node) {
+			var child, next;
+
+			switch (node.nodeType) {
+				case 1:
+					if (node.tagName.toLowerCase() != 'script') {
+						for (child = node.firstChild; child; child = next) {
+							next = child.nextSibling;
+							traverse(child);
+						}
+					}
+					break;
+
+				case 3:
+					var text = node.nodeValue;
+					var re = new RegExp(searchText, 'gi');
+					if (re.test(text)) {
+						var parent = node.parentNode;
+						var html = parent.innerHTML;
+						html = html.replace(re, '<span class="select">$&</span>');
+						parent.innerHTML = html;
+					}
+					break;
+			}
+		}
+
+		traverse(document.body);
+	}
+	setUpInput();
+	setUpMenu();
+}
+
+function interactiveSearch() {
+
+	var searchText = document.getElementById('interactive-search-text').value;
+	clearSelection();
+
+	function clearSelection() {
+		var selected = document.querySelectorAll('.select');
+		for (var i = 0; i < selected.length; i++) {
+			selected[i].classList.remove('select');
+		}
+	}
+
+	if (!searchText == "") {
+		console.log(searchText);
+
+		function traverse(node) {
+			var child, next;
+
+			switch (node.nodeType) {
+				case 1:
+					if (node.tagName.toLowerCase() != 'script') {
+						for (child = node.firstChild; child; child = next) {
+							next = child.nextSibling;
+							traverse(child);
+						}
+					}
+					break;
+
+				case 3:
+					var text = node.nodeValue;
+					var re = new RegExp(searchText, 'gi');
+					if (re.test(text)) {
+						var parent = node.parentNode;
+						var html = parent.innerHTML;
+						html = html.replace(re, '<span class="select">$&</span>');
+						parent.innerHTML = html;
+					}
+					break;
+			}
+		}
+
+		traverse(document.body);
+	}
+	setUpInput();
+	setUpMenu();
+}
+
+document.getElementById('interactive-search-text').addEventListener('input', function () {
+	interactiveSearch();
+});
+
+
+document.getElementById("daysLeft").addEventListener("click", changeTextDays);
